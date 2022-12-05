a01d0b4eb382eaf24ca0164ba02c7940e98b1bac
aragha <aras2k@yahoo.com>
2022-12-05 04:03:24 -0500

    Initial edit

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+# step 0: Initial setup
+from turtle import Screen, Turtle
+screen = Screen()
+screen.setup(height = 600, width = 600)
+screen.bgcolor('black')
+screen.title('Feed the snake!')
+
 # step 1: Create a snake body
+
+
+screen.exitonclick()
